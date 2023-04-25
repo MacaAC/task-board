ad6531a0905bccf3450335aa9379c024e05312c6
María Macarena Alvarez Castillo <mariamacarenaac@gmail.com>
2023-04-24 23:42:34 -0300

    favicon-corrections on background-correcting a bug on addTask

--- src/components/BackgroundImage/index.jsx
+++ src/components/BackgroundImage/index.jsx
@@ -1,18 +1,24 @@
 // import { Box } from "@chakra-ui/react";
-// import backgroundImage from "../../assets/bg.jpg"
-
+// import backgroundImage from "../../assets/bg.jpg";
+// import darkBackgroundImage from "../../assets/darkMode.jpg"
 // import PropTypes from 'prop-types';
+// import { useContext } from "react";
+// import { Context } from "../../context/context";
 
-// const AppBackground = ({ children }) => (
-//   <Box
-//     backgroundImage={`url(${backgroundImage})`}
-//     backgroundSize="cover"
-//     backgroundPosition="center"
-//     height="100vh"
-//   >
-//     {children}
-//   </Box>
-// );
+// const AppBackground = ({ children }) => {
+//   const context = useContext(Context);
+//   return (
+//     <Box
+//       backgroundImage={`url(${context.clearTheme ? backgroundImage : darkBackgroundImage })`}
+//       backgroundSize="100%"
+//       backgroundRepeat="repeat"
+//       backgroundPosition="center"
+//       height="100vh"
+//     >
+//       {children}
+//     </Box>
+//   );
+// }
 
 // export default AppBackground;
 
@@ -20,23 +26,17 @@
 //   children: PropTypes.node.isRequired,
 // };
 
-
-
-
 import { Box } from "@chakra-ui/react";
-import backgroundImage from "../../assets/bg.jpg";
-import darkBackgroundImage from "../../assets/darkMode.jpg"
 import PropTypes from 'prop-types';
 import { useContext } from "react";
 import { Context } from "../../context/context";
 
 const AppBackground = ({ children }) => {
   const context = useContext(Context);
+  const bgColors = context.clearTheme ? "linear-gradient(to bottom right, #F8BBD0, #FF7043)" : "linear-gradient(to bottom right, #5E35B1, #212121)";
   return (
     <Box
-      backgroundImage={`url(${context.clearTheme ? backgroundImage : darkBackgroundImage })`}
-      backgroundSize="cover"
-      backgroundPosition="center"
+      background={bgColors}
       height="100vh"
     >
       {children}
